400df094394e7933d4cb728469cc17a06ef396bd
ksurikova <ksantisurikova@gmail.com>
2025-10-15 14:00:30 +0300

    Initial Commit

---
+++ ColorBotKids/ColorBotKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorBotKidsApp.swift
+//  ColorBotKids
+//
+//  Created by Ксения Филюшкина on 15.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorBotKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
